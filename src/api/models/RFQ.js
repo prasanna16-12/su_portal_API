50ecc9f7bb3c813529fa0278cc4ff3c0fc9b63bf
Prasanna Kale <prasanna89kale@gmail.com>
2023-10-27 21:04:55 +0530

    removed vendor details from supplier RFQ

--- src/api/models/RFQ.js
+++ src/api/models/RFQ.js
@@ -235,7 +235,7 @@ module.exports = {
             //console.log(results);
             data = results[0][0];
             data["line_items"] = results[1] === undefined ? null : results[1];
-            data["vendors"] = results[2]=== undefined ? null : results[2];
+            //data["vendors"] = results[2]=== undefined ? null : results[2];
             data["Quotations"] = results[3]=== undefined ? null : results[3];
             //console.log(RFQList);
             resolve(data);
